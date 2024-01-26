17d56a57fa926e562bfb866f7c261362a449fa6b
Kevin <67015555+VienThanh12@users.noreply.github.com>
2024-01-27 00:32:47 +0200

    Reference Code

---
+++ RefCode.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define For(type, i, a, b) for (type i = (a); i <= (b); ++i)
+#define endl '\n'
+using namespace std;
+ 
+typedef long long ll;
+ 
+const ll maxN = 1e18;
+const ll minN = -1e18;
+const ll MOD = 1e9 + 7;
+ 
+const ll limit = 1e3 + 5;
+ 
+ll a[limit];
+ll n;
+
+void FastIn(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+ 
+void ReadIn(){
+  
+}
+int main(){
+    FastIn();
+    ReadIn();
+}
