03137fc33c680586f5aa8e3d52953baf4bcd422f
Kevin <67015555+VienThanh12@users.noreply.github.com>
2024-02-25 23:40:13 +0200

    Create Quicksort.cpp

---
+++ Quicksort.cpp
@@ -0,0 +1,18 @@
+void quickSort(int a[], int left, int right) {
+	int i = left, j = right;
+	int pivot = a[left + rand() % (right - left)];
+	// chia dãy thành 2 phần
+	while (i <= j) {
+		while (a[i] < pivot) ++i;
+		while (a[j] > pivot) --j;
+
+		if (i <= j) {
+			swap(a[i], a[j]);
+			++i;
+			--j;
+		}
+	}
+	// Gọi đệ quy để sắp xếp các nửa
+	if (left < j) quickSort(a, left, j);
+	if (i < right) quickSort(a, i, right);
+}
